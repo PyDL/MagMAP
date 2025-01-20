868167ccf18118e564ba6fb30791f1594d07607c
James Turtle <jturtle@predsci.com>
2025-01-20 15:16:21 -0800

    Remove .append() dataframe calls from hipft_prep.py
    - .append() is being deprecated. This is clean-up for future operability.

--- magmap/maps/hipft_prep.py
+++ magmap/maps/hipft_prep.py
@@ -27,8 +27,8 @@ def set_assim_wghts(br_map, assim_method="mu4_upton"):
         # give pixels with 'no_data_val' a 0 weight
         br_map.assim_weight[br_map.data == br_map.no_data_val] = 0
         # update layer order
-        br_map.layers = br_map.layers.append(pd.DataFrame(dict(layer_num=[1, 2], layer_name=["assim_weight", "mu"],
-                                                               py_field_name=['assim_weight', "mu"])))
+        br_map.layers = pd.concat([br_map.layers, pd.DataFrame(dict(layer_num=[1, 2], layer_name=["assim_weight", "mu"],
+                                                               py_field_name=['assim_weight', "mu"]))])
     elif assim_method == "mu4":
         # use mu^4 as the assimilation weight
         br_map.assim_weight = np.power(br_map.mu, 4)
@@ -37,8 +37,8 @@ def set_assim_wghts(br_map, assim_method="mu4_upton"):
         # give pixels with 'no_data_val' a 0 weight
         br_map.assim_weight[br_map.data == br_map.no_data_val] = 0.
         # update layer order
-        br_map.layers = br_map.layers.append(pd.DataFrame(dict(layer_num=[1, 2], layer_name=["assim_weight", "mu"],
-                                                               py_field_name=['assim_weight', "mu"])))
+        br_map.layers = pd.concat([br_map.layers, pd.DataFrame(dict(layer_num=[1, 2], layer_name=["assim_weight", "mu"],
+                                                               py_field_name=['assim_weight', "mu"]))])
     elif assim_method == "mu4_upton":
         # use mu^4 as the assimilation weight (mu <= 0.1 gets no weight)
         br_map.assim_weight = np.power(br_map.mu, 4)
@@ -47,8 +47,8 @@ def set_assim_wghts(br_map, assim_method="mu4_upton"):
         # give pixels with 'no_data_val' a 0 weight
         br_map.assim_weight[br_map.data == br_map.no_data_val] = 0
         # update layer order
-        br_map.layers = br_map.layers.append(pd.DataFrame(dict(layer_num=[1, 2], layer_name=["assim_weight", "mu"],
-                                                               py_field_name=['assim_weight', "mu"])))
+        br_map.layers = pd.concat([br_map.layers, pd.DataFrame(dict(layer_num=[1, 2], layer_name=["assim_weight", "mu"],
+                                                                    py_field_name=['assim_weight', "mu"]))])
     elif assim_method == "custom_example":
         # This example shows how to add custom layers to the map object.
         # As long as these layers are added to br_map.layers, they will be written to the
@@ -61,19 +61,19 @@ def set_assim_wghts(br_map, assim_method="mu4_upton"):
         # give pixels with 'no_data_val' a 0 weight
         br_map.assim_weight[br_map.data == br_map.no_data_val] = 0
         # update layer order
-        br_map.layers = br_map.layers.append(pd.DataFrame(dict(layer_num=[1, ], layer_name=["assim_weight", ],
-                                                               py_field_name=['assim_weight', ])))
+        br_map.layers = pd.concat([br_map.layers, pd.DataFrame(dict(layer_num=[1, ], layer_name=["assim_weight", ],
+                                                               py_field_name=['assim_weight', ]))])
         # also calculate a measurement uncertainty
         # br_map.MyLayerName must match with py_field_name=['MyLayerName', ]
         br_map.uncert = .03*br_map.data
         br_map.uncert[br_map.data == br_map.no_data_val] = br_map.no_data_val
         # update layer order
-        br_map.layers = br_map.layers.append(pd.DataFrame(dict(layer_num=[2, ], layer_name=["uncertainty", ],
-                                                               py_field_name=['uncert', ])))
+        br_map.layers = pd.concat([br_map.layers, pd.DataFrame(dict(layer_num=[2, ], layer_name=["uncertainty", ],
+                                                               py_field_name=['uncert', ]))])
 
         # pass mu as an additional layer (already exists in br_map) by adding it to the layer order
-        br_map.layers = br_map.layers.append(pd.DataFrame(dict(layer_num=[3, ], layer_name=["mu", ],
-                                                               py_field_name=['mu', ])))
+        br_map.layers = pd.concat([br_map.layers, pd.DataFrame(dict(layer_num=[3, ], layer_name=["mu", ],
+                                                               py_field_name=['mu', ]))])
     else:
         sys.exit("Assimilation method '" + assim_method + "' is not supported.")
     # record assimilation method in map object
